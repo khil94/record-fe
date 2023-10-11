9ab9dfb868a7ee8a1d7b570ce23c832b0e8a2a1d
khil94 <zidru94@gmail.com>
2023-10-12 06:15:24 +0900

    style: globalLayout 스타일링

--- src/Layouts/GlobalLayout.tsx
+++ src/Layouts/GlobalLayout.tsx
@@ -1,17 +1,17 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
+import "./GlobalLayout.scss";
 import Header from "./Header";
-
 interface Props {
   children: ReactNode;
 }
 
 export default function GlobalLayout({ children }: Props) {
   return (
-    <div>
+    <>
       <Header />
-      {children}
+      <div className="main_container">{children}</div>
       <Footer />
-    </div>
+    </>
   );
 }
