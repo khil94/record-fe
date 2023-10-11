e51a0fc6ce3d626356f3263bb843cadcce5751a0
khil94 <zidru94@gmail.com>
2023-10-12 05:51:36 +0900

    feat: react-router-dom 사용을 위한 BrowserRouter적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import GlobalLayout from "./Layouts/GlobalLayout";
 
 function App() {
-  return <GlobalLayout>메인</GlobalLayout>;
+  return (
+    <BrowserRouter>
+      <GlobalLayout>
+        <Routes>
+          <Route path="/" element={<>메인</>} />
+        </Routes>
+      </GlobalLayout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
