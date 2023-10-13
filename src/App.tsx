3a7f464972116a43dcf863cd0284829146e8bd98
khil94 <zidru94@gmail.com>
2023-10-14 00:08:38 +0900

    feat : 메인페이지를 라우팅하게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import GlobalLayout from "./Layouts/GlobalLayout";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalLayout>
         <Routes>
-          <Route path="/" element={<>메인</>} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
       </GlobalLayout>
     </BrowserRouter>
