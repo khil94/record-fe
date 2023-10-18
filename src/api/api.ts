ce17c8f8dbf3a4d7db12d1fef112e0e56036364a
khil94 <zidru94@gmail.com>
2023-10-19 05:15:40 +0900

    feat : API 기본설정

---
+++ src/api/api.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+
+// const API_CONFIG: InternalAxiosRequestConfig = {
+//   headers: {
+
+//   },
+// };
+
+const API = axios.create();
+
+const ErrorHandler = (err: AxiosError) => {
+  console.log("err", err);
+  return Promise.reject(err);
+};
+
+API.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  console.log("request");
+  return config;
+}, ErrorHandler);
+
+export default API;
