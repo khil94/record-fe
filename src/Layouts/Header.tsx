20e90b93db75227c40c78611eedeb422926e000c
khil94 <zidru94@gmail.com>
2023-10-19 05:36:08 +0900

    feat : 헤더의 SearchInput과 검색 api연동테스트

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { GetGameListBySummonerName } from "../api/apis";
 import SearchInput from "../components/SearchInput";
 import { MENU_LIST } from "../constants/MenuItem";
 import "./Header.scss";
 
 export default function Header() {
+  const [searchVal, setSearchVal] = useState("");
+
   return (
     <header className="header" id="header">
       <div className="header_inner">
@@ -20,9 +24,13 @@ export default function Header() {
           </ul>
         </nav>
         <SearchInput
-          onSubmit={() => {}}
+          onSubmit={async () => {
+            const val = searchVal.trim();
+            const resp = await GetGameListBySummonerName(val);
+            console.log(resp.data);
+          }}
           placeholder="소환사 검색"
-          onChange={(v) => console.log(v)}
+          onChange={(v) => setSearchVal(v)}
         />
       </div>
     </header>
