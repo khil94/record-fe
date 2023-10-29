83a0788a5d31616047357760b5812e99d2755123
khil94 <zidru94@gmail.com>
2023-10-29 18:41:45 +0900

    feat : 이미지위에 마우스를 호버링하면 설명이 나오는 기본 컴포넌트

---
+++ src/components/ObjImgComponent.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./ObjImgComponent.scss";
+
+interface IProp {
+  name: string;
+  description: string;
+  image: string;
+}
+
+export default function ObjImgComponent({ name, description, image }: IProp) {
+  const [showDesc, setShowDesc] = useState(false);
+
+  return (
+    <div>
+      <img
+        onMouseEnter={() => setShowDesc(true)}
+        onMouseLeave={() => setShowDesc(false)}
+        src={image}
+        alt={name}
+      />
+      {showDesc && (
+        <div className="obj_description">
+          <span className="obj_name">{name}</span>
+          <span>{description}</span>
+        </div>
+      )}
+    </div>
+  );
+}
