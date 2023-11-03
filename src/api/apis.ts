cc61847a114595d4876f0dac665a07ce605db2a7
khil94 <zidru94@gmail.com>
2023-11-03 16:37:14 +0900

    chore : api에 반환타입 명시

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,12 +1,13 @@
+import { ISimpleMatch, ISummonerProfile } from "../types/types";
 import API from "./api";
 
 export const GetSummonerInfo = async (summonerName: string) => {
-  const resp = await API.get(`/api/summoner/${summonerName}`);
+  const resp = await API.get<ISummonerProfile>(`/api/summoner/${summonerName}`);
   return resp;
 };
 
 export const GetGameList = async (puid: string, pageNumber: number) => {
-  const resp = await API.get(`/api/matches/${puid}`, {
+  const resp = await API.get<ISimpleMatch[]>(`/api/matches/${puid}`, {
     params: {
       page: pageNumber,
     },
