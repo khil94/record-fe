1b32075d965f870d4978b61f0414eb898165e5e4
khil94 <zidru94@gmail.com>
2023-11-03 19:56:11 +0900

    feat : Loading 컴포넌트 prop받을 수 있게 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,13 @@
 import "./Loading.scss";
 
-export default function Loading() {
+interface IProp extends React.HTMLAttributes<HTMLImageElement> {
+  width?: number;
+}
+
+export default function Loading({ width, ...rest }: IProp) {
   return (
     <div className="Loading_Wrapper">
-      <img alt="loading" src="/public/Loading.gif" />
+      <img width={width} {...rest} alt="loading" src="/public/Loading.gif" />
     </div>
   );
 }
