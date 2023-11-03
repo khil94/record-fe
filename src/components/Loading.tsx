6868e3293f166a814fcb6ef0fcb1b20e493cdb5f
khil94 <zidru94@gmail.com>
2023-11-03 19:46:39 +0900

    feat : 로딩컴포넌트

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import "./Loading.scss";
+
+export default function Loading() {
+  return (
+    <div className="Loading_Wrapper">
+      <img alt="loading" src="/public/Loading.gif" />
+    </div>
+  );
+}
