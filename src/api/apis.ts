0ba05b48e48054c05e4c12370fee901b2504a684
khil94 <zidru94@gmail.com>
2023-11-12 17:52:07 +0900

    fix : 랭킹정보 api 반환타입 오류 수정

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -1,4 +1,9 @@
-import { ISimpleMatch, ISummonerProfile } from "../types/types";
+import {
+  ILeaderBoardQueueTyep,
+  IRanking,
+  ISimpleMatch,
+  ISummonerProfile,
+} from "../types/types";
 import API from "./api";
 
 export const GetSummonerInfo = async (summonerName: string) => {
@@ -10,6 +15,19 @@ export const GetGameList = async (puid: string, pageNumber: number) => {
   const resp = await API.get<ISimpleMatch[]>(`/api/matches/${puid}`, {
     params: {
       page: pageNumber,
+    },
+  });
+  return resp;
+};
+
+export const GetRankingList = async (
+  queueType: ILeaderBoardQueueTyep,
+  pageNumber: number
+) => {
+  const resp = await API.get<IRanking>(`/api/leaderboard`, {
+    params: {
+      page: pageNumber,
+      queue: queueType,
     },
   });
   return resp;
