9c0ba1cbb8e63e83653304c5756e1b0fc626c91f
khil94 <zidru94@gmail.com>
2023-11-12 21:13:47 +0900

    feat : 랭킹페이지에서 솔로/자유랭크 종류를 나누어 볼 수 있게 하는 기능 추가

--- src/pages/RankingPage.tsx
+++ src/pages/RankingPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { GetRankingList } from "../api/apis";
 import { ILeaderBoardQueueTyep, IRanking } from "../types/types";
+import "./RankingPage.scss";
 
 export default function RankingPage() {
   const [rnkData, setRnkData] = useState<IRanking>();
@@ -16,63 +17,87 @@ export default function RankingPage() {
       }
     };
     getData();
-  }, []);
+  }, [currentQueueType]);
 
   return (
     <div className="ranking_page">
-      <table>
-        <colgroup>
-          <col width={40} />
-          <col />
-          <col width={110} />
-          <col width={60} />
-          <col width={50} />
-          <col width={172} />
-        </colgroup>
-        <thead>
-          <th align="left" scope="col">
-            순위
-          </th>
-          <th align="left" scope="col">
-            소환사
-          </th>
-          <th align="left" scope="col">
-            티어
-          </th>
-          <th align="left" scope="col">
-            LP
-          </th>
-          <th align="left" scope="col">
-            레벨
-          </th>
-          <th align="left" scope="col">
-            승률
-          </th>
-        </thead>
-        <tbody>
-          {rnkData?.players.map((v, i) => {
-            return (
-              <tr key={v.summonerName}>
-                {Object.keys(v).map((t) => {
-                  if (t !== "summonerId") {
-                    return (
-                      <td align="left" key={v + t}>
-                        {v[t]}
-                      </td>
-                    );
-                  } else {
-                    return (
-                      <td align="left" key={v + t}>
-                        {i + 1}
-                      </td>
-                    );
-                  }
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="select_queue_type">
+        <div
+          className={
+            currentQueueType === "RANKED_SOLO_5x5" ? "selected_tab" : "tab"
+          }
+          onClick={() => {
+            setCurrentQueueType("RANKED_SOLO_5x5");
+          }}
+        >
+          솔로 랭크
+        </div>
+        <div
+          className={
+            currentQueueType === "RANKED_FLEX_SR" ? "selected_tab" : "tab"
+          }
+          onClick={() => {
+            setCurrentQueueType("RANKED_FLEX_SR");
+          }}
+        >
+          자유 랭크
+        </div>
+      </div>
+      <div className="ranking_content">
+        <table>
+          <colgroup>
+            <col width={40} />
+            <col />
+            <col width={110} />
+            <col width={60} />
+            <col width={50} />
+            <col width={172} />
+          </colgroup>
+          <thead>
+            <th align="left" scope="col">
+              순위
+            </th>
+            <th align="left" scope="col">
+              소환사
+            </th>
+            <th align="left" scope="col">
+              티어
+            </th>
+            <th align="left" scope="col">
+              LP
+            </th>
+            <th align="left" scope="col">
+              레벨
+            </th>
+            <th align="left" scope="col">
+              승률
+            </th>
+          </thead>
+          <tbody>
+            {rnkData?.players.map((v, i) => {
+              return (
+                <tr key={v.summonerName}>
+                  {Object.keys(v).map((t) => {
+                    if (t !== "summonerId") {
+                      return (
+                        <td align="left" key={v + t}>
+                          {v[t]}
+                        </td>
+                      );
+                    } else {
+                      return (
+                        <td align="left" key={v + t}>
+                          {i + 1}
+                        </td>
+                      );
+                    }
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
