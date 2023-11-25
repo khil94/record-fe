b34b6b216f9e0a4c965402fcc4d2b8e7a427e968
khil94 <zidru94@gmail.com>
2023-11-25 23:56:11 +0900

    feat : 표기할 데이터가 없다면 최근 전적을 표기하지 않게 수정

--- src/components/UserRecentInfoComponent.tsx
+++ src/components/UserRecentInfoComponent.tsx
@@ -89,13 +89,17 @@ export default function UserRecentInfoComponent({ userData }: IProp) {
   }
   return (
     <div className="summoner_champ_info">
-      <div className="summoner_recent_header">
-        <span>최근 전적</span>
-        <span>{`${userData.length}전 ${win}승 ${
-          userData.length - win
-        }패`}</span>
-      </div>
-      <UserRecentChampComponent {...data} />
+      {data.length && (
+        <>
+          <div className="summoner_recent_header">
+            <span>최근 전적</span>
+            <span>{`${userData.length}전 ${win}승 ${
+              userData.length - win
+            }패`}</span>
+          </div>
+          <UserRecentChampComponent {...data} />
+        </>
+      )}
     </div>
   );
 }
