cd429c7c58ce30cec40334baa4c0b9ae73824d22
khil94 <zidru94@gmail.com>
2023-11-25 20:05:33 +0900

    feat : 필요없어진 api들 삭제

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -7,31 +7,10 @@ import {
 } from "../types/types";
 import API from "./api";
 
-export const commonFetcher = (url: string) =>
-  API.get(url).then((res) => res.data);
-
-export const GetSummonerInfo = async (summonerName: string) => {
-  const resp = await API.get<ISummonerProfile>(`/api/summoner/${summonerName}`);
-  return resp;
-};
-
 export const GetGameList = async (puid: string, pageNumber: number) => {
   const resp = await API.get<ISimpleMatch[]>(`/api/matches/${puid}`, {
     params: {
       page: pageNumber,
-    },
-  });
-  return resp;
-};
-
-export const GetRankingList = async (
-  queueType: ILeaderBoardQueueTyep,
-  pageNumber: number
-) => {
-  const resp = await API.get<IRanking>(`/api/leaderboard`, {
-    params: {
-      page: pageNumber,
-      queue: queueType,
     },
   });
   return resp;
