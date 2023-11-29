a0f82aa3fefa5c345755c2bcf6bf11e0136d873f
khil94 <zidru94@gmail.com>
2023-11-29 20:06:08 +0900

    chore : 프록시설정 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          // "http://ec2-54-180-159-190.ap-northeast-2.compute.amazonaws.com:8080/",
-          "http://api.lolstat.net/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: false,
-        ws: true,
-      },
-    },
-  },
 });
