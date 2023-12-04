7d64e538c222268b9d7e6e52338c2aedabc3d33a
khil94 <zidru94@gmail.com>
2023-12-04 22:11:30 +0900

    feat : 최근검색기록을 통해 해당 소환사 검색/검색기록 삭제기능 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import SearchInput from "../components/SearchInput";
-import { addRecentSearchVal } from "../utils/generalFunctions";
+import {
+  addRecentSearchVal,
+  deleteRecentSearchVal,
+} from "../utils/generalFunctions";
 import "./MainPage.scss";
 
 export default function MainPage() {
   const [searchVal, setSearchVal] = useState("");
   const [showRecent, setShowRecent] = useState(false);
   const navigation = useNavigate();
-  const recentSearchVal = localStorage.getItem("recent")
-    ? JSON.parse(localStorage.getItem("recent")!)
-    : [];
+  const [recentSearchVal, setRecentSearchVal] = useState(
+    localStorage.getItem("recent")
+      ? JSON.parse(localStorage.getItem("recent")!)
+      : []
+  );
 
   function RecentSearch({ val }: { val: string }) {
     return (
-      <div className="recent_search_val">
+      <div
+        onMouseDown={() => {
+          navigation(`summoner/${val}`);
+        }}
+        className="recent_search_val"
+      >
         <span>{val}</span>
-        <span>X</span>
+        <span
+          onMouseDown={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            setRecentSearchVal(recentSearchVal.filter((v) => v !== val));
+            deleteRecentSearchVal(val);
+          }}
+          className="delete_recent"
+        >
+          X
+        </span>
       </div>
     );
   }
@@ -39,16 +59,14 @@ export default function MainPage() {
         placeholder="소환사 검색"
         onChange={(v) => setSearchVal(v)}
       />
-      {showRecent && (
-        <div className="recent_search_val_wrapper">
-          <div className="recent_search_info">최근 검색 목록</div>
-          <div className="recent_search_val_container">
-            {recentSearchVal.map((v) => {
-              return <RecentSearch val={v} />;
-            })}
-          </div>
+      <div className={`recent_search_val_wrapper ${showRecent ? "show" : ""}`}>
+        <div className="recent_search_info">최근 검색 목록</div>
+        <div className="recent_search_val_container">
+          {recentSearchVal.map((v) => {
+            return <RecentSearch key={v} val={v} />;
+          })}
         </div>
-      )}
+      </div>
     </div>
   );
 }
