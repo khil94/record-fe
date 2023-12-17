d21d64bb22724275e21761a3fc4b59e2bf18b577
khil94 <zidru94@gmail.com>
2023-12-17 20:15:35 +0900

    [style] : 로그인 페이지 스타일링 작업

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,12 +3,23 @@ import "./LoginPage.scss";
 export default function LoginPage() {
   return (
     <div className="login_page_wrapper">
+      <div className="logo_wrapper">
+        <img src="logo.png" width={88} />
+      </div>
       <div className="login_form_wrapper">
-        <form>
-          <input type="email" placeholder="이메일" />
-          <input type="password" placeholder="비밀번호" />
-          <button type="submit">제출</button>
+        <form id="login_form" className="login_form">
+          <div className="input_wrapper">
+            <input required id="login_email" />
+            <label htmlFor="login_email">이메일</label>
+          </div>
+          <div className="input_wrapper">
+            <input required id="login_password" type="password" />
+            <label htmlFor="login_password">비밀번호</label>
+          </div>
         </form>
+        <button form="login_form" className="btn_submit" type="submit">
+          제출
+        </button>
       </div>
     </div>
   );
