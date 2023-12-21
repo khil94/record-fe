d3a55ceff2dfe09c3dac3cb3a5c0d26310ffea1f
khil94 <zidru94@gmail.com>
2023-12-22 04:41:39 +0900

    [feat] : 로그인 페이지 api 임시연결

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,29 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { PostLogin } from "../api/apis";
+import CommonModal from "../components/CommonModal";
 import "./LoginPage.scss";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const navigator = useNavigate();
+
+  async function HandleLogin() {
+    try {
+      const resp = await PostLogin(email, pwd);
+      navigator("/");
+    } catch {
+      setShowModal(true);
+      console.log("hi");
+    }
+  }
+
+  function resetForm() {
+    setEmail("");
+    setPwd("");
+  }
 
   return (
     <div className="login_page_wrapper">
@@ -12,13 +31,29 @@ export default function LoginPage() {
         <img src="logo.png" width={88} />
       </div>
       <div className="login_form_wrapper">
-        <form id="login_form" className="login_form">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            HandleLogin();
+          }}
+          id="login_form"
+          className="login_form"
+        >
           <div className="input_wrapper">
-            <input required id="login_email" />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              id="login_email"
+            />
             <label htmlFor="login_email">이메일</label>
           </div>
           <div className="input_wrapper">
-            <input required id="login_password" type="password" />
+            <input
+              onChange={(e) => setPwd(e.target.value)}
+              required
+              id="login_password"
+              type="password"
+            />
             <label htmlFor="login_password">비밀번호</label>
           </div>
         </form>
@@ -40,6 +75,15 @@ export default function LoginPage() {
           </Link>
         </div>
       </div>
+      <CommonModal
+        showModal={showModal}
+        title="로그인 실패"
+        message="로그인 오류가 발생했습니다. 정보를 다시 확인해주세요."
+        onDisapppear={() => {
+          resetForm();
+          setShowModal(false);
+        }}
+      />
     </div>
   );
 }
