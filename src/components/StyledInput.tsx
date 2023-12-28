c2437db64a9f2c3e30bda870c32f54ec2776c75d
khil94 <zidru94@gmail.com>
2023-12-28 16:17:48 +0900

    [feat] : StyledInput 이 일반 input의 attr을 받고, err 메세지도 입력가능하게 변경

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -1,8 +1,10 @@
 import "./StyledInput.scss";
 
-interface IProp {
-  onChange: (v: string) => void;
+interface IProp extends React.InputHTMLAttributes<HTMLInputElement> {
   required?: boolean;
+  err?: boolean;
+  errMsg?: string;
+  label?: string;
   value: string;
 }
 
@@ -10,15 +12,23 @@ export default function StyledInput({
   onChange,
   required = false,
   value,
+  err,
+  errMsg,
+  label = "",
+  ...rest
 }: IProp) {
   return (
     <div className="input_wrapper">
       <input
-        onChange={(e) => onChange(e.target.value)}
+        className={`${value.length === 0 ? "" : !err ? "valid" : "invalid"}`}
+        onChange={onChange}
         required={required}
+        value={value}
+        {...rest}
         id="styled_input"
       />
-      <label htmlFor="styled_input">{value}</label>
+      <label htmlFor="styled_input">{label}</label>
+      {err && value.length > 0 && <span className="error_msg">{errMsg}</span>}
     </div>
   );
 }
