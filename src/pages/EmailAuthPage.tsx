3ed7d0eb60de34455c85d2e40c14c3e4e7ac7d4b
khil94 <zidru94@gmail.com>
2023-12-30 03:38:46 +0900

    [feat] : 이메일 페이지 api 연동 및 에러코드 핸들링

--- src/pages/EmailAuthPage.tsx
+++ src/pages/EmailAuthPage.tsx
@@ -1,13 +1,15 @@
+import axios from "axios";
 import { useState } from "react";
-import API from "../api/api";
 import { PutVerifyEmail } from "../api/apis";
 import CommonModal from "../components/CommonModal";
 import StyledInput from "../components/StyledInput";
+import { IError } from "../types/types";
 import "./EmailAuthPage.scss";
 
 export default function EmailAuthPage() {
   const [authNum, setAuthNum] = useState("");
   const [showModal, setShowModal] = useState(false);
+  const [msg, setMsg] = useState("");
 
   return (
     <div className="email_auth_page_wrapper">
@@ -22,16 +24,29 @@ export default function EmailAuthPage() {
           onSubmit={async (e) => {
             e.preventDefault();
             try {
-              console.log(API.defaults);
               await PutVerifyEmail(authNum);
             } catch (e) {
-              console.log(e);
+              if (axios.isAxiosError<IError>(e)) {
+                switch (e.response?.data.errorCode) {
+                  case 9000:
+                    setMsg("인증번호가 잘못되었습니다. 다시 확인해주세요.");
+                    break;
+                  case 1004:
+                    setMsg(
+                      "인증번호의 유효기간이 만료되었습니다. 재발급된 메일을 확인해주세요."
+                    );
+                    break;
+                  default:
+                    setMsg("이메인 인증에 실패하였습니다. 다시 확인해주세요.");
+                    break;
+                }
+              }
               setShowModal(true);
             }
           }}
         >
           <StyledInput
-            onChange={(e) => setAuthNum(e.target.value)}
+            onChange={(e) => setAuthNum(e.target.value.trim())}
             value={authNum}
             label="인증 번호"
           />
@@ -41,7 +56,7 @@ export default function EmailAuthPage() {
       <CommonModal
         showModal={showModal}
         title="인증 실패"
-        message="이메인 인증에 실패하였습니다. 다시 확인해주세요."
+        message={msg}
         onDisapppear={() => setShowModal(false)}
       />
     </div>
