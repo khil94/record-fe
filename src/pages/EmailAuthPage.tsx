5b8642e1e8bfa95a2e99fc5d40beecc6a7220b97
khil94 <zidru94@gmail.com>
2023-12-29 19:38:46 +0900

    [feat] : 이메일 인증 화면 최종 목업

--- src/pages/EmailAuthPage.tsx
+++ src/pages/EmailAuthPage.tsx
@@ -1,30 +1,29 @@
 import { useState } from "react";
-import useUser from "../utils/useUser";
+import StyledInput from "../components/StyledInput";
 import "./EmailAuthPage.scss";
 
 export default function EmailAuthPage() {
-  const [authNumSent, setAuthNumSent] = useState(false);
-  const { data } = useUser();
+  const [authNum, setAuthNum] = useState("");
 
   return (
     <div className="email_auth_page_wrapper">
-      <h1>이메일 인증이 필요합니다.</h1>
+      <h1>이메일 인증</h1>
+      <div className="subtitle">
+        <span>이메일 인증이 완료되지 않은 계정입니다.</span>
+        <span>먼저 이메일 인증을 완료해 주세요</span>
+      </div>
       <div>
-        <form>
-          <input disabled={!authNumSent} placeholder="인증 번호" />
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              if (authNumSent) {
-                setAuthNumSent(false);
-              } else {
-                setAuthNumSent(true);
-              }
-            }}
-            type={authNumSent ? "submit" : "button"}
-          >
-            {authNumSent ? "남은 시간" : "인증키 발송"}
-          </button>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <StyledInput
+            onChange={(e) => setAuthNum(e.target.value)}
+            value={authNum}
+            label="인증 번호"
+          />
+          <button type="submit">제출하기</button>
         </form>
       </div>
     </div>
