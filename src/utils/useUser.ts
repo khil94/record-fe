aafd4442f7d47035ed38592ca48ede0224efcc5b
khil94 <zidru94@gmail.com>
2023-12-30 08:07:13 +0900

    [feat] : 불필요한 코드 삭제(useUser)

--- src/utils/useUser.ts
+++ src/utils/useUser.ts
@@ -20,11 +20,11 @@ export default function useUser() {
   const logout = async () => {
     API.defaults.headers.common["Authorization"] = "";
     localStorage.removeItem("user");
-    await mutate({ auth: false }, { revalidate: false });
+    await mutate({ auth: false });
   };
 
   const login = async () => {
-    await mutate({ auth: true }, { revalidate: false });
+    await mutate({ auth: true });
   };
 
   return { data, logout, login };
