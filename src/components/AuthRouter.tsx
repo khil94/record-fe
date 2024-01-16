d1e76c45e154b2dcb87d365f1fd0f921eae3d6b9
khil94 <zidru94@gmail.com>
2024-01-16 18:09:57 +0900

    [feat] : authRouter 개선

--- src/components/AuthRouter.tsx
+++ src/components/AuthRouter.tsx
@@ -1,5 +1,5 @@
-import { ReactNode, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
 import useAuth from "../utils/useAuth";
 
 interface IProp {
@@ -7,18 +7,22 @@ interface IProp {
   reverse?: boolean;
 }
 
+function getXOR(a: boolean, b: boolean) {
+  if (!a && b) {
+    return true;
+  }
+  if (a && !b) {
+    return true;
+  }
+  return false;
+}
+
 export default function AuthRouter({ children, reverse = false }: IProp) {
   const { data } = useAuth();
-  const navigator = useNavigate();
 
-  useEffect(() => {
-    if (data?.auth && reverse) {
-      navigator("/login");
-    }
-    if (!data?.auth && !reverse) {
-      navigator("/login");
-    }
-  }, [data?.auth, reverse]);
-
-  return <>{children}</>;
+  return getXOR(data?.auth || false, reverse) ? (
+    <>{children}</>
+  ) : (
+    <Navigate to={"/login"} />
+  );
 }
