f68ad065997aab038743192347d15081e81bbd91
khil94 <zidru94@gmail.com>
2024-01-25 19:04:53 +0900

    [style] : styledInput 크기조절이 용이하게 변경

--- src/components/StyledInput.tsx
+++ src/components/StyledInput.tsx
@@ -21,17 +21,19 @@ export default function StyledInput({
   ...rest
 }: IProp) {
   return (
-    <div className={`input_wrapper ${mode === "common" ? "" : "dark"}`}>
-      <input
-        className={`${value.length === 0 ? "" : !err ? "valid" : "invalid"}`}
-        onChange={onChange}
-        required={required}
-        value={value}
-        {...rest}
-        id={`styled_input_${label}`}
-      />
-      <label htmlFor="styled_input">{label}</label>
-      {err && value.length > 0 && <span className="error_msg">{errMsg}</span>}
+    <div className="input_outer_wrapper">
+      <div className={`input_wrapper ${mode === "common" ? "" : "dark"}`}>
+        <input
+          className={`${value.length === 0 ? "" : !err ? "valid" : "invalid"}`}
+          onChange={onChange}
+          required={required}
+          value={value}
+          {...rest}
+          id={`styled_input_${label}`}
+        />
+        <label htmlFor="styled_input">{label}</label>
+        {err && value.length > 0 && <span className="error_msg">{errMsg}</span>}
+      </div>
     </div>
   );
 }
